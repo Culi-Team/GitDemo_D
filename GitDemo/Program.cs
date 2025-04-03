0cd92816858103a5db14dd36b7a002d7a9cfc5fb
DatNguyen <thanhdatbk1994@gmail.com>
2025-04-03 11:22:08 +0700

    Dat 3

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Xin chao, day la Dat");
+Console.WriteLine("Xin chao, day la Dat3");
