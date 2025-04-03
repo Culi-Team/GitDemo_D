179c2670adb575f6845d119474d342377dfad64e
NGUYEN PHU TRONG <trongong123@gmail.com>
2025-04-03 16:32:46 +0700

    Group name

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -3,3 +3,5 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Xin chao, day la Dat");
 Console.WriteLine("Test nhanh");
 Console.WriteLine("Xin chao, day la Dat3");
+string GroupName = "쿨리팀";
+Console.WriteLine(GroupName);
